782563804780f1fc468b41d568f7a0d520156eac
Eduardo Janini <eduardojanini@gmail.com>
2026-01-03 21:35:41 -0300

    Ingestão do ticket_sla_events

---
+++ raw_layer/ticket_sla_events.py
@@ -0,0 +1,35 @@
+import requests
+import psycopg2
+import json
+
+API_URL = "http://localhost:8000/ticket_sla_events"
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="datawarehouse",
+    ticket_sla_event="postgres",
+    password="admin"
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+drop table if exists raw_ticket_sla_events cascade;   
+create table if not exists raw_ticket_sla_events (payload jsonb);
+""")
+
+response = requests.get(API_URL)
+response.raise_for_status()
+
+ticket_sla_events = response.json()
+
+for ticket_sla_event in ticket_sla_events.get("data", []):
+    cur.execute(
+        "insert into raw_ticket_sla_events (payload) values (%s)",
+        [json.dumps(ticket_sla_event)]
+    )
+
+conn.commit()
+cur.close()
+conn.close()
