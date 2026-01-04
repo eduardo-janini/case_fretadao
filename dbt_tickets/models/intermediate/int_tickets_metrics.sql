02aff792e3cdccf4182f9ab96cb7ba8206782e76
Eduardo Janini <eduardojanini@gmail.com>
2026-01-03 21:13:20 -0300

    Ajuste na camada intermediária

--- dbt_tickets/models/intermediate/int_tickets_metrics.sql
+++ dbt_tickets/models/intermediate/int_tickets_metrics.sql
@@ -1,32 +1,34 @@
-with reference as (
+{{ config(materialized='view') }}
 
+
+with reference as (
     select
         ticket_id,
         custom_fields
-    from {{ ref('stg_tickets') }}
-
+    from 
+        {{ ref('stg_tickets') }}
 ),
 
 exploded as (
-
     select
         ticket_id,
-        (field ->> 'id')::int     as field_id,
-        field ->> 'value'         as field_value
-    from reference
-    cross join lateral jsonb_array_elements(custom_fields) as field
+        (field ->> 'id')::int as field_id,
+        field ->> 'value'     as field_value
+    from
+        reference
+        cross join lateral jsonb_array_elements(custom_fields) as field
 ),
 
 filtro as (
-
-    select *
-    from exploded
-    where field_id between 1 and 5
-
+    select
+        *
+    from 
+        exploded
+    where
+        field_id between 1 and 5
 ),
 
-pivotagem as (
-
+pivot as (
     select
         ticket_id,
 
@@ -35,30 +37,32 @@ pivotagem as (
         max(case when field_id = 3 then field_value end) as tipo_contrato,
         max(case when field_id = 4 then field_value end) as tipo_veiculo,
         max(case when field_id = 5 then field_value end) as tipo_fretamento
-
-    from filtro
-    group by ticket_id
-
+    from
+        filtro
+    group by
+        ticket_id
 )
 
-select 
-    ticket_id,
-    assignee_id,
-    group_id,
-    organization_id,
-    created_at,
-    updated_at,
-    channel,
-    status,
-    priority,
-    type as ticket_type,
-    description,
-    url,
-    public,
-    tipo_servico,
-    desc_regiao,
-    tipo_contrato,
-    tipo_veiculo,
-    tipo_fretamento
-from {{ ref('stg_tickets') }} as stg
-    left join pivotagem as piv using(ticket_id)
+select
+    stg.ticket_id,
+    stg.assignee_id,
+    stg.group_id,
+    stg.organization_id,
+    stg.created_at,
+    stg.updated_at,
+    stg.channel,
+    stg.status,
+    stg.priority,
+    stg.type as ticket_type,
+    stg.description,
+    stg.url,
+    stg.public,
+
+    piv.tipo_servico,
+    piv.desc_regiao,
+    piv.tipo_contrato,
+    piv.tipo_veiculo,
+    piv.tipo_fretamento
+from 
+    {{ ref('stg_tickets') }} as stg
+    left join pivot as piv using(ticket_id)
